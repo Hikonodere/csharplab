1c065d4929fd7cf197431fe17398dc107be17a23
Narozhnik Aliaksandr <narozniksasa@gmail.com>
2025-10-28 23:35:04 +0300

    Ceating a method for add sweets, implementation of IComparable interface

--- ConsoleApp4/ConsoleApp4/Sweets/Sweets.cs
+++ ConsoleApp4/ConsoleApp4/Sweets/Sweets.cs
@@ -1,7 +1,7 @@
 ﻿using System.Data;
 namespace ConsoleApp4.Sweets
 {
-    public abstract class Sweets
+    public abstract class Sweets : IComparable<Sweets>
     {
         public String Name { get; private set; }
 
@@ -36,8 +36,52 @@ namespace ConsoleApp4.Sweets
         {
             return Compound.ContainsKey("sugar") ? Compound["sugar"] : 0;
         }
+
+        public static Sweets CreateSweetFromUserInput()
+        {
+            Console.WriteLine("Выберите тип сладости:");
+            Console.WriteLine("1 - Шоколад");
+            Console.WriteLine("2 - Карамель");
+            Console.WriteLine("3 - Желейная конфета");
+            Console.WriteLine("4 - Печенье");
+
+            var choice = Console.ReadLine();
+
+            Console.Write("Введите название: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Введите вес (г): ");
+            var weight = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите содержание сахара (г): ");
+            var sugar = int.Parse(Console.ReadLine());
+            if (sugar < 0)
+            {
+                throw new ArgumentException("Некоректное содержание сахара");
+            }
+
+            var compound = new Dictionary<string, int> { ["sugar"] = sugar };
+
+            return choice switch
+            {
+                "1" => Chocolate.CreateChocolateFromInput(name, compound, weight),
+                "2" => Caramel.CreateCaramelFromInput(name, compound, weight),
+                "3" => JellyCandy.CreateJellyFromInput(name, compound, weight),
+                "4" => Cookie.CreateCookieFromInput(name, compound, weight),
+                _ => throw new ArgumentException("Неверный выбор")
+            };
+
+        }
         public override string ToString() => $"{GetType().Name}: {Name}";
 
+        public int CompareTo(Sweets other)
+        {
+            if (other == null) return 1;
+            return Weight.CompareTo(other.Weight);
+        }
+
+
         public abstract string GetDescription();
+
     }
 }
